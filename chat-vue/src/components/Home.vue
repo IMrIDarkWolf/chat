92b1b975f9d77e618dbafb06d3152ed23ed0396f
Anatolii <jaeki94@gmail.com>
2019-03-19 10:41:27 +0200

    fixed vue-project

---
+++ chat-vue/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Чат на vue.js</h1>
+    <button @click="goLogin">Вход</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    goLogin () {
+      this.$router.push({name: "login"})
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ chat-vue/src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input v-model="login" placeholder="Логин">
+    <input v-model="password" placeholder="Пароль">
+    <button>Войти</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      login: '',
+      password: '',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
